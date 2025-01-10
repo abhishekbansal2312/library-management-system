2c82ab3d3fc1b043eef8ca630b99532973c2563d
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-01-11 04:24:35 +0530

    improved filtering in borrowRecords Routes

--- library-management-backend/controllers/borrowController.js
+++ library-management-backend/controllers/borrowController.js
@@ -2,7 +2,10 @@ const BorrowRecords = require("../models/BorrowBook");
 
 const getBorrowRecords = async (req, res) => {
   try {
-    const borrowRecords = await BorrowRecords.find().populate("book user");
+    const borrowRecords = await BorrowRecords.find()
+      .populate("book", "title author genre price")
+      .populate("user", "name email");
+
     res.json(borrowRecords);
   } catch (error) {
     res.status(500).json({ message: "Error fetching borrow records" });
