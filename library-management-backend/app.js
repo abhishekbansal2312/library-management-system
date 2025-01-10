72972ac28f6491cd2d8ad3b3bf088a81d3c0c717
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-01-11 04:14:27 +0530

    added Borrowed Book routes

--- library-management-backend/app.js
+++ library-management-backend/app.js
@@ -8,12 +8,14 @@ dotenv.config();
 const bookRoutes = require("./routes/bookRoutes");
 const authorRoutes = require("./routes/authorRoutes");
 const userRoutes = require("./routes/userRoutes");
+const borrowRoutes = require("./routes/borrowRoutes");
 
 app.use(express.json());
 
 app.use("/books", bookRoutes);
 app.use("/authors", authorRoutes);
 app.use("/users", userRoutes);
+app.use("/borrow-records", borrowRoutes);
 
 app.listen(port, () => {
   console.log("Server is running on port http://localhost:3000");
--- library-management-backend/controllers/borrowController.js
+++ library-management-backend/controllers/borrowController.js
@@ -2,7 +2,7 @@ const BorrowRecords = require("../models/BorrowBook");
 
 const getBorrowRecords = async (req, res) => {
   try {
-    const borrowRecords = await BorrowRecords.find().populate();
+    const borrowRecords = await BorrowRecords.find().populate("book user");
     res.json(borrowRecords);
   } catch (error) {
     res.status(500).json({ message: "Error fetching borrow records" });
